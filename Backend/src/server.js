f78490e63e3db2db9e046ce08525fc36ff2a6ff3
Mathushank <mathushan12.k@gmail.com>
2025-03-05 22:58:46 +0530

    changed the port to 5001

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -18,5 +18,5 @@ app.get("/", (req, res) => {
 import userRoutes from "./routes/userRoutes.js";
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
