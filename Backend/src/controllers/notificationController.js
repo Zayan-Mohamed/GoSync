2b8cdd537a0bd23ce057dca16c2a9438c744f069
Nilukshika <niluroche@gmail.com>
2025-03-05 09:13:19 -0800

    backend create function

---
+++ Backend/src/controllers/notificationController.js
@@ -0,0 +1,13 @@
+import Notification from "../models/notificationModel.js";
+
+
+export const create = async (req, res) => {
+  try {
+      const newNotification = new Notification(req.body);
+      const savedNotification = await newNotification.save();
+      res.status(201).json({ success: true, data: savedNotification });
+  } catch (error) {
+      console.error(error); // Log the error for debugging
+      res.status(500).json({ errorMessage: error.message, message: 'Server Error' });
+  }
+};
