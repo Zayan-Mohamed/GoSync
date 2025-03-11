53e48304642896111949ecb284bc5f59606b784e
Zayan Mohamed <itsm.zayan@gmail.com>
2025-03-11 11:19:38 +0530

    uuid installed

--- Backend/src/models/bookingModel.js
+++ Backend/src/models/bookingModel.js
@@ -2,30 +2,25 @@ import mongoose from "mongoose";
 
 const bookingSchema = mongoose.Schema(
   {
-    bookingId: {
-      type: String,
-      unique: true,
-      required: true,
-    },
+    bookingId: { type: String, unique: true, required: true },
     passenger: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-    bus: {
+    bus: { type: mongoose.Schema.Types.ObjectId, ref: "Bus", required: true },
+    route: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Bus",
+      ref: "Route",
       required: true,
     },
-    seatNumber: {
-      type: Number,
+    scheduleId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Schedule",
       required: true,
     },
-    status: {
-      type: String,
-      enum: ["booked", "cancelled"],
-      default: "booked",
-    },
+    seatNumbers: [{ type: Number, required: true }],
+    status: { type: String, enum: ["booked", "cancelled"], default: "booked" },
   },
   { timestamps: true }
 );
