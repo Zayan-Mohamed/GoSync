67a941dcfc2a143dc2c924c64f417a60a30c407b
Zayan Mohamed <itsm.zayan@gmail.com>
2025-03-19 20:08:01 +0530

    Register and Login page alterations

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -1,13 +1,25 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import AdminDashboard from "../pages/AdminDashboard";
 import PassengerDashboard from "../pages/PassengerDashboard";
 import ProtectedRoute from "../components/ProtectedRoute";
+import SplashScreen from "../pages/SplashScreen";
 
 const AppRoutes = () => {
+
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setShowSplash(false), 3000);
+  }, []);
+
   return (
     <Router>
+      {showSplash ? (
+        <SplashScreen />
+      ) : (
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
@@ -25,6 +37,7 @@ const AppRoutes = () => {
         {/* ✅ Catch-all for unauthorized access */}
         <Route path="/unauthorized" element={<h1>Access Denied</h1>} />
       </Routes>
+      )}
     </Router>
   );
 };
