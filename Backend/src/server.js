041e0d7a68204950137f0fdb1c752e70eacb9b51
Nilukshika <niluroche@gmail.com>
2025-03-20 14:00:57 +0530

    socket.io

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import bodyParser from "body-parser";
+import http from "http";
+import { Server } from "socket.io";
 
 import scheduleRoutes from "./routes/scheduleRoutes.js"; // ✅ Import schedule routes
 import userRoutes from "./routes/userRoutes.js"; // ✅ Import user routes
@@ -19,12 +21,17 @@ dotenv.config();
 connectDB();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+
 
 // ✅ Middleware
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+app.set("io", io);
 // ✅ Test Route
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -42,3 +49,5 @@ app.use("/api/buses", busRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export { io };
